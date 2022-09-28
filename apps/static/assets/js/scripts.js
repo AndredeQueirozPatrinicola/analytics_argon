3c97cb2866d1cfe08bcd43a7ba0a3c5aa373d212
André de Queiroz P <andrepatrinicola@gmail.com>
2022-09-28 10:23:16 -0300

    fix: corrige bug de docentes sem artigos

--- apps/static/assets/js/scripts.js
+++ apps/static/assets/js/scripts.js
@@ -1,6 +1,21 @@
 $(document).ready(function () {
-    $('#livros').removeClass('show').removeClass('active');
-    $('#capitulos').removeClass('show').removeClass('active');
+
+    let artigo = document.getElementById('artigo');
+    let livros = document.getElementById('livros');
+    let capitulos = document.getElementById('capitulos');
+
+    if(artigo === null){
+        $('#capitulos').removeClass('show').removeClass('active');
+        $('#grafico-livros').addClass('active')
+    }else if(livros === null){
+        $('#grafico-capitulos').addClass('active')
+    }else{
+        $('#livros').removeClass('show').removeClass('active');
+        $('#capitulos').removeClass('show').removeClass('active');
+    }
+
     $('#loading').css('display', 'none'); 
+
 });
+
 
