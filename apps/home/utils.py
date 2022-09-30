ad1318b034c0ee4ff0a5e4cefb5c2e37c767fcc8
André de Queiroz P <andrepatrinicola@gmail.com>
2022-09-30 09:34:45 -0300

    refactor: corrige script de popular db para atualizar tambem

--- apps/home/utils.py
+++ apps/home/utils.py
@@ -19,120 +19,146 @@ class Api:
         self.api_pesquisa = self.api + 'pesquisa'
 
     def pega_dados_departamentos(self):
-        siglas = ['FLA', 'FLP', 'FLF', 'FLH', 'FLC', 'FLM', 'FLO', 'FLL', 'FSL', 'FLT', 'FLG']
-        anos = [2016,2017,2018,2019,2020,2021]
+        print("Inicio")
+        print("Checando Apis...")
+        siglas = ['FLA', 'FLP', 'FLF', 'FLH', 'FLC',
+                  'FLM', 'FLO', 'FLL', 'FSL', 'FLT', 'FLG']
+        anos = [2016, 2017, 2018, 2019, 2020, 2021]
 
-        departamentos_siglas = {'FLA': 'Antropologia', 'FLP': 'Ciência Política', 'FLF': 'Filosofia', 'FLH': 'História', 'FLC': "Letras Clássicas e Vernáculas",
-                                'FLM': "Letras Modernas", 'FLO': 'Letras Orientais', 'FLL': 'Lingüística', 'FSL': 'Sociologia', 'FLT': "Teoria Literária e Literatura Comparada", 'FLG': 'Geografia'}
+        departamentos_siglas = {
+            'FLA': 'Antropologia', 'FLP': 'Ciência Política', 'FLF': 'Filosofia', 'FLH': 'História', 'FLC': "Letras Clássicas e Vernáculas",
+            'FLM': "Letras Modernas", 'FLO': 'Letras Orientais', 'FLL': 'Lingüística', 'FSL': 'Sociologia', 
+            'FLT': "Teoria Literária e Literatura Comparada", 'FLG': 'Geografia'
+            }
 
         parametros = {
-            'filtro' : 'serie_historica',
-            'ano_ini' : 2016,
-            'ano_fim' : 2021,
-            'serie_historica_tipo' : 'departamento'
+            'filtro': 'serie_historica',
+            'ano_ini': 2016,
+            'ano_fim': 2021,
+            'serie_historica_tipo': 'departamento'
         }
-        
+
         parametros_vazios = {
-            'filtro' : 'departamento',
-            'ano_ini' : '',
-            'ano_fim' : '',
-            'serie_historica_tipo' : ''
+            'filtro': 'departamento',
+            'ano_ini': '',
+            'ano_fim': '',
+            'serie_historica_tipo': ''
         }
 
-        raw_programas = requests.get('https://dados.fflch.usp.br/api/programas')
+        raw_programas = requests.get(
+            'https://dados.fflch.usp.br/api/programas')
         raw_docentes = requests.get('https://dados.fflch.usp.br/api/docentes')
-        raw_pesquisa = requests.get(url='https://dados.fflch.usp.br/api/pesquisa', params=parametros_vazios)
-        raw_pesquisa_parametros = requests.get(url = f'https://dados.fflch.usp.br/api/pesquisa', params=parametros)
+        raw_pesquisa = requests.get(
+            url='https://dados.fflch.usp.br/api/pesquisa', params=parametros_vazios)
+        raw_pesquisa_parametros = requests.get(
+            url=f'https://dados.fflch.usp.br/api/pesquisa', params=parametros)
 
-
-
-        dados_programas = raw_programas.json() # dicionario com dicionarios # FEITO
-        dados_docentes = raw_docentes.json()   # lista com dicionarios # FEITO
-        dados_pesquisa = raw_pesquisa.json()   # dicionarios com dicionario  FEITO
-        dados_pesquisa_parametros = raw_pesquisa_parametros.json() #dicionarios com dicionarios
+        dados_programas = raw_programas.json()  
+        dados_docentes = raw_docentes.json()   
+        dados_pesquisa = raw_pesquisa.json()   
+        dados_pesquisa_parametros = raw_pesquisa_parametros.json()
 
         x = 0
         while x < len(siglas):
-                
 
-            raw_programas_docente_limpo = requests.get(f'https://dados.fflch.usp.br/api/programas/docentes/{siglas[x]}')
+            raw_programas_docente_limpo = requests.get(
+                f'https://dados.fflch.usp.br/api/programas/docentes/{siglas[x]}')
             dados_programas_docentes_limpo = raw_programas_docente_limpo.json()
             api_programas_docentes_limpo = dados_programas_docentes_limpo
 
-
             # Dicionario com dados de pesquisa por departamento
-            siglas_dic = {siglas[x] : dados_pesquisa.get(siglas[x])}
+            dados_pesquisa_tratados = {siglas[x]: dados_pesquisa.get(siglas[x])}
             print(siglas[x])
-            
-            
+
             # Lista com dicionario com dados dos docentes de cada departamento
-            dados_docentes_filtrados = [i for i in dados_docentes if i.get('nomset') == departamentos_siglas.get(siglas[x])]
-            
-            
+            dados_docentes_filtrados = [i for i in dados_docentes if i.get(
+                'nomset') == departamentos_siglas.get(siglas[x])]
+
             # Lista com dicionarios por ano
             for i in dados_pesquisa_parametros:
                 if i == departamentos_siglas.get(siglas[x]):
                     lista_pesquisa_por_ano = [dados_pesquisa_parametros.get(i)]
-            
-            
-            
+
             # Lista de dicionarios com os dados por departamento da api de programas
             dados_dep = dados_programas.get('departamentos')
             z = 0
             for i in dados_dep:
                 if dados_dep[z].get('sigla') == siglas[x]:
-                    # Resultado a ser salvo
                     lista_programas_departamentos = [i]
 
                 z += 1
-            
-            
-            
-            # Bloco que pega dados dos anos 2016 - 2021 sobre produção 
+
+            # Bloco que pega dados dos anos 2016 - 2021 sobre produção
             y = 0
             lista_programas_docentes = []
             while y < len(anos):
                 sleep(1)
-                parametros_programas = {'tipo' : 'anual', 'ano' : anos[y], 'ano_ini' : '', 'ano_fim' : ''}
+                parametros_programas = {
+                    'tipo': 'anual', 'ano': anos[y], 'ano_ini': '', 'ano_fim': ''}
 
-                print(anos[y])
+                print(f"Dados de {anos[y]} salvos")
                 raw_programas_docentes = requests.get(
                     url=f'https://dados.fflch.usp.br/api/programas/docentes/{siglas[x]}', params=parametros_programas)
 
-                dados_programas_docentes = raw_programas_docentes.json()  # FEITO
+                dados_programas_docentes = raw_programas_docentes.json()  
 
                 # Lista com dados da produção de todos os docentes por ano (2016-2021)
-                dicionario_dados = {anos[y]: dados_programas_docentes}
+                dicionario_dados = {str(anos[y]): dados_programas_docentes}
                 lista_programas_docentes.append(dicionario_dados)
-                
-            
+
                 y += 1
 
+
+            verifica_api_docentes = Departamento.objects.filter(sigla = siglas[x]).values_list('api_docentes')
+            verifica_api_programas = Departamento.objects.filter(sigla = siglas[x]).values_list('api_programas')
+            verifica_programas_docente = Departamento.objects.filter(sigla = siglas[x]).values_list('api_programas_docente')
+            verifica_api_pesquisa = Departamento.objects.filter(sigla = siglas[x]).values_list('api_pesquisa')
+            verifica_api_pesquisa_parametros = Departamento.objects.filter(sigla = siglas[x]).values_list('api_pesquisa_parametros')
+            verifica_api_programas_docente_limpo = Departamento.objects.filter(sigla = siglas[x]).values_list('api_programas_docente_limpo')
+
+            verifica_existencia = Departamento.objects.filter(sigla=siglas[x])
+
+            if verifica_existencia.exists():
+
+                if verifica_api_docentes[0][0] != dados_docentes_filtrados:
+                    print("Api docentes com diferença")
+                    Departamento.objects.filter(sigla=siglas[x]).update(api_docentes=dados_docentes_filtrados)
+
+                if verifica_api_programas[0][0] != lista_programas_departamentos:
+                    print("Api programas com diferença")
+                    Departamento.objects.filter(sigla=siglas[x]).update(api_programas=lista_programas_departamentos)
                 
+                if verifica_programas_docente[0][0] != lista_programas_docentes:
+                    print("Api programas docente com diferença")
+                    Departamento.objects.filter(sigla=siglas[x]).update(api_programas_docente=lista_programas_docentes)
                 
-            dados = [siglas_dic, 
-            lista_programas_docentes, 
-            lista_programas_departamentos, 
-            lista_pesquisa_por_ano, 
-            dados_docentes_filtrados]
+                if verifica_api_pesquisa[0][0] != dados_docentes_filtrados:
+                    print("Api pesquisa com diferença")
+                    Departamento.objects.filter(sigla=siglas[x]).update(api_pesquisa=dados_docentes_filtrados)
 
+                if verifica_api_pesquisa_parametros[0][0] != lista_pesquisa_por_ano:
+                    print("Api pesquisa por ano com diferença")
+                    Departamento.objects.filter(sigla=siglas[x]).update(api_pesquisa_parametros=lista_pesquisa_por_ano)
 
+                if verifica_api_programas_docente_limpo[0][0] != api_programas_docentes_limpo:
+                    print("Api programas docente limpo com diferença")
+                    Departamento.objects.filter(sigla=siglas[x]).update(api_programas_docente_limpo=api_programas_docentes_limpo)
 
-            dados = Departamento(sigla = siglas[x], 
-                                api_docentes=dados_docentes_filtrados,
-                                api_programas=lista_programas_departamentos, 
-                                api_programas_docente=lista_programas_docentes,
-                                api_pesquisa=siglas_dic, 
-                                api_pesquisa_parametros = lista_pesquisa_por_ano,
-                                api_programas_docente_limpo=api_programas_docentes_limpo)
-            
-            dados.save()
+                print(f"{siglas[x]} atualizado com sucesso")
+
+            else:
+                dados = Departamento(sigla=siglas[x],
+                                     api_docentes=dados_docentes_filtrados,
+                                     api_programas=lista_programas_departamentos,
+                                     api_programas_docente=lista_programas_docentes,
+                                     api_pesquisa=dados_docentes_filtrados,
+                                     api_pesquisa_parametros=lista_pesquisa_por_ano,
+                                     api_programas_docente_limpo=api_programas_docentes_limpo)
+
+                dados.save()
+                print(f"{siglas[x]} salvo com sucesso")
 
             x += 1
-
-
-
-
 
     def pega_dados_docente(self):
         print('Inicio')
@@ -140,16 +166,16 @@ class Api:
         dados = res.json()
         df = pd.DataFrame(dados)
         parametros = df['id_lattes'].to_list()
-        
 
         z = 0
         while z < len(parametros):
-            sleep(2)
+            sleep(3)
             parametro = parametros[z]
 
-            docentes = requests.get(url=self.api_programas_docentes + parametro)
+            docentes = requests.get(
+                url=self.api_programas_docentes + parametro)
             programas = requests.get(url=self.api_programas)
-            
+
             docentes_dados = docentes.json()
             programas_dados = programas.json()
 
@@ -161,27 +187,47 @@ class Api:
             while x < len(dados_departamentos):
                 if parametro in dados_departamentos[x].get('id_lattes_docentes'):
                     lista_api.append(dados_departamentos[x])
-                
+
                 x += 1
-            
+
             if lista_api == []:
-                lista_api.append(dados_programas)
-                
-                #verifica = Docente.objects.filter(docente_id=parametros[z])
-
-                #verifica.update(api_programas=lista_api)
-                
-
+                lista_api.append(dados_programas) 
             
+            for i in dados: 
+                if i.get('id_lattes') == parametros[z]:
+                    dados_api_docentes = i
 
-            salva_json = Docente(docente_id=parametro,
-                                    api_docente=docentes_dados, api_programas=lista_api)
 
-            salva_json.save()
-            print(f'Salvou: {parametros[z]} | {z} de {len(parametros)}')
+
+            verifica_api_docente = Docente.objects.filter(docente_id=parametros[z]).values_list('api_docente')
+            verifica_api_programas = Docente.objects.filter(docente_id=parametros[z]).values_list('api_programas')
+            verifica_api_docentes = Docente.objects.filter(docente_id=parametros[z]).values_list('api_docentes')
+
+            verifica_existencia = Docente.objects.filter(docente_id = parametros[z])
+
+            if verifica_existencia.exists():
+
+                if docentes_dados != verifica_api_docente[0][0]:
+                    print("Api docente: Atualizada!")
+                    verifica_api_docente = Docente.objects.filter(docente_id=parametros[z]).update(api_docente=docentes_dados)
+
+                if lista_api != verifica_api_programas[0][0]:
+                    print("Api programas: Atualizada!")
+                    verifica_api_docente = Docente.objects.filter(docente_id=parametros[z]).update(api_programas=lista_api)
+
+                if dados_api_docentes != verifica_api_docentes[0][0]:
+                    print("Api docentes: Atualizada!")
+                    verifica_api_docente = Docente.objects.filter(docente_id=parametros[z]).update(api_docentes=dados_api_docentes)
+
+                print(f'Atualizou: {parametros[z]} | {z+1} de {len(parametros)}')
+
+            else:
+                salva_json = Docente(docente_id=parametro,
+                                    api_docente=docentes_dados, 
+                                    api_programas=lista_api, 
+                                    api_docentes=dados_api_docentes)
+
+                salva_json.save()
+                print(f'Salvou: {parametros[z]} | {z+1} de {len(parametros)}')
+
             z += 1
-
-        
-
-            
-
