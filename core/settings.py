09f12fc6aef87eb8479b1a42c8d69b73f7870614
André de Queiroz P <andrepatrinicola@gmail.com>
2022-10-17 09:22:25 -0300

    feat: adiciona django-toolbar

--- core/settings.py
+++ core/settings.py
@@ -22,6 +22,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'debug_toolbar',
     'apps.home',  # Enable the inner home (home)
 ]
 
@@ -34,6 +35,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 ROOT_URLCONF = 'core.urls'
@@ -120,3 +122,17 @@ if DEBUG:
 else:
     STATIC_ROOT = os.path.join(CORE_DIR, 'apps/static')
 
+
+
+INTERNAL_IPS = [
+    # ...
+    "127.0.0.1",
+    # ...
+]
+
+
+
+if DEBUG:
+    import socket 
+    hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
+    INTERNAL_IPS = [ip[: ip.rfind(".")] + ".1" for ip in ips] + ["127.0.0.1", "10.0.2.2"]
