9d9b2ee15112f8e44622598627901950010851db
André de Queiroz <andrepatrinicola@gmail.com>
2022-11-24 13:24:25 -0300

    refactor: troca nome de variaveis

--- services/populadb/Docentes.py
+++ services/populadb/Docentes.py
@@ -8,15 +8,15 @@ from apps.home.models import Docente
 
 class ApiDocente:
 
-    def __init__(self, numero_usp: str):
-        self.numero_usp = numero_usp
+    def __init__(self, numero_lattes: str):
+        self.numero_lattes = numero_lattes
 
     def pega_api_docentes(self):
         res = requests.get('https://dados.fflch.usp.br/api/docentes')
         dados = res.json()
 
         for i in dados: 
-                if i.get('id_lattes') == self.numero_usp:
+                if i.get('id_lattes') == self.numero_lattes:
                     dados_api_docentes = i
 
         return dados_api_docentes
@@ -31,7 +31,7 @@ class ApiDocente:
         x = 0
         dados_api_programas = []
         while x < len(dados_departamentos):
-            if self.numero_usp in dados_departamentos[x].get('id_lattes_docentes'):
+            if self.numero_lattes in dados_departamentos[x].get('id_lattes_docentes'):
                 dados_api_programas.append(dados_departamentos[x])
 
             x += 1
@@ -42,13 +42,13 @@ class ApiDocente:
         return dados_api_programas
 
     def pega_api_docente(self):
-        res = requests.get('https://dados.fflch.usp.br/api/programas/docente/' + self.numero_usp)
+        res = requests.get('https://dados.fflch.usp.br/api/programas/docente/' + self.numero_lattes)
         dados = res.json()
         return dados
 
 
     def atualiza_ou_cria_docente(self, api_docente, api_programas, api_docentes):
-        docente = Docente.objects.filter(docente_id = self.numero_usp)
+        docente = Docente.objects.filter(docente_id = self.numero_lattes)
         
         if docente.exists():
         
@@ -60,7 +60,7 @@ class ApiDocente:
         else:
 
             docente = Docente(
-                                docente_id=self.numero_usp,
+                                docente_id=self.numero_lattes,
                                 api_docente = api_docente,
                                 api_programas = api_programas,
                                 api_docentes = api_docentes
