f5b1365b39615746f84ed700efba3a6ec856ce79
André de Queiroz <andrepatrinicola@gmail.com>
2023-04-12 12:41:11 -0300

    fix: corrige click event dropdown

--- apps/frontend/src/dropdown.js
+++ apps/frontend/src/dropdown.js
@@ -9,11 +9,12 @@ async function addDropdownListener() {
         elementChildren[0].addEventListener('click', function () {
             dropdown(elementChildren[1])
         })
-    }
-    window.onclick = (event) => {
 
-        if (!event.target.matches('span') && !event.target.matches('.dropdown-items') && !event.target.matches('.dropdown-button') && !event.target.matches('.fa-arrow-down')) {
-            element.children[1].classList.remove("dropdown-is-toggled")
+        window.onclick = (event) => {
+
+            if (!event.target.matches('span') && !event.target.matches('.dropdown-items') && !event.target.matches('.dropdown-button') && !event.target.matches('.fa-arrow-down')) {
+                element.children[1].classList.remove("dropdown-is-toggled")
+            }
         }
     }
 }
